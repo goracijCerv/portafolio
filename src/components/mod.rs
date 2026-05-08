d5995570fdde715a1a90c178095864112b92c42f
goracijCerv <goracijpcervantilia@gmail.com>
2026-05-08 14:26:39 -0600

    cambio en cargo para poder hacerlo funcional

--- src/components/mod.rs
+++ src/components/mod.rs
@@ -1,8 +1,8 @@
 // pub mod contact;
 // pub mod footer;
-// pub mod hero;
+pub mod hero;
 pub mod loading;
-// pub mod navigation;
+pub mod navigation;
 // pub mod projects;
 // pub mod skills;
 // pub mod terminal;
